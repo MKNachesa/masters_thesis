b9ee68da557b045ab7b397f4533f7b460830802f
MKNachesa <maya.nachesa@gmail.com>
2023-02-20 13:28:04 +0100

    data bucketing now includes gender

--- scripts/bucket_data.py
+++ scripts/bucket_data.py
@@ -18,18 +18,35 @@ start = df["min_age"].min() # 24
 end = df["min_age"].max()   # 73
 bucket_ranges = [(i, i+4) for i in range(start, end, 5)]
 
-id_to_min_age = df.groupby("intressent_id").agg("mean")["min_age"].to_dict()
+id_to_min_age = df.groupby("intressent_id").agg(
+    lambda x: list(x)[0])[["min_age", "gender"]].to_dict(orient="index")
 
-buckets = [list() for i in range(len(bucket_ranges))]
+buckets = [set() for i in range(len(bucket_ranges))]
 
-for iid, min_age in id_to_min_age.items():
+max_bucket_size = 4
+
+bucket_genders = [{"F": 0, "M": 0} for i in range(len(bucket_ranges))]
+
+# first pass for gender balance
+for iid, d in id_to_min_age.items():
+    min_age, gender = d.values()
     i = int((min_age-start)//5)
-    if len(buckets[i]) < 4:
-        buckets[i].append(iid)
+    if len(buckets[i]) < max_bucket_size \
+       and bucket_genders[i][gender] < max_bucket_size//2:
+        buckets[i].add(iid)
+        bucket_genders[i][gender] += 1
+
+# second pass to fill buckets (some buckets otherwise don't have 4 speakers)
+for iid, d in id_to_min_age.items():
+    min_age, gender = d.values()
+    i = int((min_age-start)//5)
+    if len(buckets[i]) < max_bucket_size and iid not in buckets[i]:
+        buckets[i].add(iid)
+        bucket_genders[i][gender] += 1
 
 range_to_bucket = dict(zip(bucket_ranges, buckets))
 
-ids_to_use = set(reduce(lambda x, y: x + y, range_to_bucket.values()))
+ids_to_use = set(reduce(lambda x, y: x.union(y), range_to_bucket.values()))
 
 df["to_use"] = df["intressent_id"].apply(lambda x: x in ids_to_use)
 
