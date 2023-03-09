b033f7697e5654af3f9ebd9386f5dce329b495d9
MKNachesa <maya.nachesa@gmail.com>
2023-03-09 13:58:39 +0100

    updated graphs, refactored code

--- scripts/embs_within_age_within_speaker.py
+++ scripts/embs_within_age_within_speaker.py
@@ -6,91 +6,93 @@ import pickle as pkl
 from sklearn.metrics.pairwise import cosine_similarity
 from functools import reduce
 import matplotlib.pyplot as plt
-
-NUM_PAIRS = 6
+import numpy as np
 
 scripts_dir = os.getcwd()
 os.chdir("../data")
 data_dir = os.getcwd()
 os.chdir("../results")
 results_dir = os.getcwd()
+os.chdir("../metadata")
+meta_dir = os.getcwd()
 os.chdir(scripts_dir)
 
-speech_lengths = ["full", 60, 30, 10, 5, 3, 1]
-# df = pd.read_parquet("../metadata/within_speaker_within_age_comparisons.parquet")
-df = pd.read_parquet("../metadata/within_speaker_across_age_comparisons.parquet")
-danfs_to_use = set()
+def get_cossim_df(num_pairs, parquet_path, save_path):
+    print(f"Processing {save_path}")
 
-for i in range(1, NUM_PAIRS+1):
-    danfs_to_use.update(set(reduce(lambda x, y: list(x) + list(y), list(map(set, df[f"pair_{i}"].tolist())))))
-
-dokids_to_use = set(i.split("_")[0] for i in danfs_to_use)
-
-for speech_length in speech_lengths:
-    print(f"Processing {speech_length}")
-    length_dir = os.path.join(data_dir, str(speech_length))
-    embs = dict()
+    NUM_PAIRS = num_pairs
+    df = pd.read_parquet(os.path.join(meta_dir, parquet_path))
 #
-    for emb_path in next(os.walk(length_dir))[2]:
-        if emb_path.split("_")[1].split(".")[0] in dokids_to_use:
-            with open(os.path.join(length_dir, emb_path), "rb") as infile:
-                dokid_embs = pkl.load(infile)
-            for danf, emb in dokid_embs.items():
-                if danf in danfs_to_use:
-                    embs[danf] = emb
-#
+    danfs_to_use = set()
     for i in range(1, NUM_PAIRS+1):
-        df[f"score_{i}_{speech_length}"] = df[f"pair_{i}"].apply(lambda y: cosine_similarity(*list(map(lambda x: embs[x], y)))[0][0])
+        danfs_to_use.update(set(reduce(lambda x, y: list(x) + list(y), list(map(set, df[f"pair_{i}"].tolist())))))
 #
-    df[f"score_mean_{speech_length}"] = df[[f"score_{i}_{speech_length}" for i in range(1, NUM_PAIRS+1)]].apply(lambda x: sum(x)/NUM_PAIRS, axis=1)
+    dokids_to_use = set(i.split("_")[0] for i in danfs_to_use)
+#
+    for speech_length in speech_lengths:
+        print(f"Processing {speech_length}")
+        length_dir = os.path.join(data_dir, str(speech_length))
+        embs = dict()
+        for emb_path in next(os.walk(length_dir))[2]:
+            if emb_path.split("_")[1].split(".")[0] in dokids_to_use:
+                with open(os.path.join(length_dir, emb_path), "rb") as infile:
+                    dokid_embs = pkl.load(infile)
+                for danf, emb in dokid_embs.items():
+                    if danf in danfs_to_use:
+                        embs[danf] = emb
+        for i in range(1, NUM_PAIRS+1):
+            df[f"score_{i}_{speech_length}"] = df[f"pair_{i}"].apply(lambda y: cosine_similarity(*list(map(lambda x: embs[x], y)))[0][0])
+        df[f"score_mean_{speech_length}"] = df[[f"score_{i}_{speech_length}" for i in range(1, NUM_PAIRS+1)]].apply(lambda x: sum(x)/NUM_PAIRS, axis=1)
+#
+    for speech_length in speech_lengths:
+        plt.hist(df[f"score_mean_{speech_length}"].tolist())
+        plt.title(f'{(" ").join(save_path.split("_"))} cosine similarity scores for {speech_length} speech length')
+        plt.savefig(os.path.join(results_dir, f"{save_path}_{speech_length}_cossim_score.png"))
+        plt.close()
+#
+    lesbian_flag = [(213, 45, 0), (239, 118, 39), (255, 154, 86), (230, 230, 230), (209, 98, 164), (181, 86, 144), (163, 2, 98)]
+    for i, speech_length in enumerate(speech_lengths):
+        plt.hist(df[f"score_mean_{speech_length}"].tolist(), label=f"{speech_length}",
+        bins=np.arange(0, 1, 0.02), color=tuple(map(lambda x: x/255, lesbian_flag[i]))),
+    plt.title(f'{(" ").join(save_path.split("_"))} cosine similarity scores')
+    plt.legend()
+    plt.savefig(os.path.join(results_dir, f"{save_path}_all_cossim_score.png"))
+    plt.close()
+    
+    print()
+    return df
 
+speech_lengths = ["full", 60, 30, 10, 5, 3, 1]
+
+across_age_df = get_cossim_df(num_pairs=3,
+                              parquet_path="within_speaker_across_age_comparisons.parquet",
+                              save_path="within_speaker_across_age")
+
+within_age_df = get_cossim_df(num_pairs=3,
+                              parquet_path="within_speaker_within_age_comparisons.parquet",
+                              save_path="within_speaker_within_age")
+
+across_speaker_df = get_cossim_df(num_pairs=1,
+                                  parquet_path="across_speaker_comparisons.parquet",
+                                  save_path="across_speaker")
+
+NUM_PAIRS = 3
 for speech_length in speech_lengths:
-    plt.hist(df[f"score_mean_{speech_length}"].tolist())
-    # plt.show()
-    plt.savefig(os.path.join(results_dir, f"within_speaker_across_age_{speech_length}_cossim_score.png"))
+    plt.hist(list(reduce(lambda x, y: x + y, [across_age_df[f"score_{i}_{speech_length}"].tolist() for i in range(1, NUM_PAIRS)])),
+             label="within speakers across ages")
+    plt.hist(across_speaker_df[f"score_1_{speech_length}"].tolist(),
+             label="across speakers")
+    plt.legend()
+    plt.title(f'across speaker VS within speaker across ages cosine similarity scores for {speech_length} speech length')
+    plt.savefig(os.path.join(results_dir, f"withiin_speaker_across_age_VS_across_speaker_{speech_length}_cossim_score.png"))
     plt.close()
 
-
-
-# across speakers
-NUM_PAIRS = 1
-speech_lengths = ["full", 60, 30, 10, 5, 3, 1]
-
-df_across = pd.read_parquet("../metadata/across_speaker_comparisons_small.parquet")
-# df_across["pair_1"] = df_across["pair1"]
-# df_across.drop("pair1", inplace=True, axis=1)
-danfs_to_use = set()
-df_across[["danf1", "danf2"]] = pd.DataFrame(df_across["pair_1"].tolist(), index=df_across.index)
-
-danfs_to_use = set(df_across.danf1.tolist()).union(set(df_across.danf2.tolist()))
-dokids_to_use = set(map(lambda x: x.split("_")[0], set(df_across.danf1.tolist()))).union(set(map(lambda x: x.split("_")[0], set(df_across.danf2.tolist()))))
-
 for speech_length in speech_lengths:
-    print(f"Processing {speech_length}")
-    length_dir = os.path.join(data_dir, str(speech_length))
-    embs = dict()
-#
-    for emb_path in next(os.walk(length_dir))[2]:
-        if emb_path.split("_")[1].split(".")[0] in dokids_to_use:
-            with open(os.path.join(length_dir, emb_path), "rb") as infile:
-                dokid_embs = pkl.load(infile)
-            for danf, emb in dokid_embs.items():
-                if danf in danfs_to_use:
-                    embs[danf] = emb
-#
-    for i in range(1, NUM_PAIRS+1):
-        df_across[f"score_{i}_{speech_length}"] = df_across[f"pair_{i}"].apply(lambda y: cosine_similarity(*list(map(lambda x: embs[x], y)))[0][0])
-#
-    # df_across[f"score_mean_{speech_length}"] = df_across[[f"score_{i}_{speech_length}" for i in range(1, NUM_PAIRS+1)]].apply(lambda x: sum(x)/NUM_PAIRS, axis=1)
-
-for speech_length in speech_lengths:
-    plt.hist(df_across[f"score_1_{speech_length}"].tolist())
-    plt.savefig(os.path.join(results_dir, f"across_speaker_{speech_length}_cossim_score.png"))
-    plt.close()
-
-NUM_PAIRS = 6
-for speech_length in speech_lengths:
-    plt.hist(list(reduce(lambda x, y: x + y, [df[f"score_{i}_{speech_length}"].tolist() for i in range(1, NUM_PAIRS)])))
-    plt.hist(df_across[f"score_1_{speech_length}"].tolist())
-    plt.savefig(os.path.join(results_dir, f"within_speaker_across_age_VS_across_speaker_{speech_length}_cossim_score.png"))
+    plt.hist(list(reduce(lambda x, y: x + y, [within_age_df[f"score_{i}_{speech_length}"].tolist() for i in range(1, NUM_PAIRS)])),
+             label="within speakers within ages")
+    plt.hist(across_speaker_df[f"score_1_{speech_length}"].tolist(),
+             label="across speakers")
+    plt.legend()
+    plt.title(f'across speaker VS within speaker within ages cosine similarity scores for {speech_length} speech length')
+    plt.savefig(os.path.join(results_dir, f"within_speaker_within_age_VS_across_speaker_{speech_length}_cossim_score.png"))
     plt.close()
--- scripts/pipeline_demo.py
+++ scripts/pipeline_demo.py
@@ -6,127 +6,101 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 # within speaker within age
 NUM_PAIRS = 3
+AGE_RANGE = 10
 
 df = pd.read_parquet("../metadata/bucketed_speeches.parquet")
 
 ids_to_dokanf = df.groupby(
     ["intressent_id", "age"]).agg(list)["dokid_anfnummer"].to_dict()
 
+# get starting ages for each speaker
+ids_to_min_age = dict()
+for iid, age in ids_to_dokanf.keys():
+    if age < ids_to_min_age.get(iid, 100):  
+            ids_to_min_age[iid] = age
+
+# for each speaker, only keep speakers + debates within range of (starting_age, starting_age + AGE_RANGE-1) (e.g. 20-29)
+ids_to_dokanf = dict(filter(lambda x: x[0][1] < ids_to_min_age[x[0][0]] + AGE_RANGE, ids_to_dokanf.items()))
+
 pairs = defaultdict(set)
 
-# check that this doesn't contain doubles both ways!!!
 # first pass
-# make as many pairs as possible where the two speeches *don't* come from the same debate
+# for each speaker at each age, find pairs of speeches such that they *don't* come from the same debate
 for ids, dokid_anfnummers in ids_to_dokanf.items():
     tmp_dokid_anfnummers = dokid_anfnummers[:]
     dokids = set(i.split("_")[0] for i in tmp_dokid_anfnummers)
-    while len(dokids) > 1:
+#
+    # find speeches from different debates
+    # stop if all remaining speeches are in the same debate or there are NUM_PAIRS pairs
+    while len(dokids) > 1 and len(pairs[ids]) < NUM_PAIRS:
         danf1 = choice(tmp_dokid_anfnummers)
         danf2 = choice(tmp_dokid_anfnummers)
         while danf1.split("_")[0] == danf2.split("_")[0]:
             danf2 = choice(tmp_dokid_anfnummers)
-        _ = tmp_dokid_anfnummers.pop(tmp_dokid_anfnummers.index(danf1))
-        _ = tmp_dokid_anfnummers.pop(tmp_dokid_anfnummers.index(danf2))
+#
+        del tmp_dokid_anfnummers[tmp_dokid_anfnummers.index(danf1)]
+        del tmp_dokid_anfnummers[tmp_dokid_anfnummers.index(danf2)]
         pairs[ids].add(tuple(sorted([danf1, danf2])))
-        if len(pairs[ids]) == NUM_PAIRS:
-            break
+#
         dokids = set([i.split("_")[0] for i in tmp_dokid_anfnummers])
 
-print(set([len(pair) for pair in pairs.values()]))
-
-# second pass (include speeches from the same debate)
+# second pass
+# for some speakers it isn't possible to find pairs such that for each both speeches come from a different debate
+# fill up speeches pairs for within-speaker comparisons such that all speakers at all ages have NUM_PAIRS comparison pairs
 for ids, dokid_anfnummers in ids_to_dokanf.items():
     while len(pairs[ids]) < NUM_PAIRS:
         new_pair = set()
         while len(new_pair) < 2:
             new_pair.add(choice(dokid_anfnummers))
-        # new_pair = (choice(dokid_anfnummers), choice(dokid_anfnummers))
         pairs[ids].add(tuple(sorted(list(new_pair))))
 
-print(set([len(pair) for pair in pairs.values()]))
 
-# new_df = pd.DataFrame(pairs, columns=["intressent_id", "age"] + [f"column_{i+1}" for i in range(6)])
+# save the pairs to a dataframe
 new_df = pd.DataFrame.from_dict(pairs, columns=[f"pair_{i+1}" for i in range(NUM_PAIRS)], orient="index")
 new_df = new_df.reset_index()
 new_df[["intressent_id", "age"]] = pd.DataFrame(new_df["index"].tolist(), index=new_df.index)
 new_df = new_df.drop("index", axis=1)
 new_df = new_df[["intressent_id", "age"] + [f"pair_{i+1}" for i in range(NUM_PAIRS)]]
-print(new_df)
 
 new_df.to_parquet("../metadata/within_speaker_within_age_comparisons.parquet")
 
 
 # across speakers
-# this takes forever 💀
-if False:
-    comparisons = dict()
-    i = 0
-    for ids1, dokid_anfnummers1 in ids_to_dokanf.items():
-        if (i+1) % 20 == 0:
-            print(i+1)
-        for ids2, dokid_anfnummers2 in ids_to_dokanf.items():
-            if ids1[0] == ids2[0]:
-                continue
-            dokids = set([i.split("_")[0] for i in dokid_anfnummers1] + [i.split("_")[0] for i in dokid_anfnummers2])
+# create speech pairs between different speakers
+comparisons = dict()
+NUM_PAIRS = 3
+
+def get_pair(dokid_anfnummers1, dokid_anfnummers2):
+    """returns a pair of speeches such that, if possible, they do not come from the same debate"""
+#
+    dokids = set([i.split("_")[0] for i in dokid_anfnummers1] + [i.split("_")[0] for i in dokid_anfnummers2])
+    danf1 = choice(dokid_anfnummers1)
+    danf2 = choice(dokid_anfnummers2)
+    if len(dokids) > 1:
+        while (danf1 == danf2) or (danf1.split("_")[0] == danf2.split("_")[0]):
             danf1 = choice(dokid_anfnummers1)
             danf2 = choice(dokid_anfnummers2)
-            if len(dokids) > 1:
-                while danf1.split("_")[0] == danf2.split("_")[0]:
-                    danf1 = choice(dokid_anfnummers1)
-                    danf2 = choice(dokid_anfnummers2)
-            pair = (danf1, danf2)
-            while tuple(sorted(list(pair))) in set(comparisons.values()):
-                danf1 = choice(dokid_anfnummers1)
-                danf2 = choice(dokid_anfnummers2)
-                if len(dokids) > 1:
-                    while danf1.split("_")[0] == danf2.split("_")[0]:
-                        danf1 = choice(dokid_anfnummers1)
-                        danf2 = choice(dokid_anfnummers2)
-                pair = (danf1, danf2)
-            comparisons[(ids1, ids2)] = pair
-        i += 1
+    # else:
+    #     while (danf1 == danf2):
+    #         danf1 = choice(dokid_anfnummers1)
+    #         danf2 = choice(dokid_anfnummers2)
+    pair = (danf1, danf2)
+    return pair
 
-    new_df = pd.DataFrame.from_dict(comparisons, columns=["danf1", "danf2"], orient="index")
-    new_df = new_df.reset_index()
-    new_df[["pair1", "pair2"]] = pd.DataFrame(new_df["index"].tolist(), index=new_df.index)
-    new_df[["intressent_id1", "age1"]] = pd.DataFrame(new_df["pair1"].tolist(), index=new_df.index)
-    new_df[["intressent_id2", "age2"]] = pd.DataFrame(new_df["pair2"].tolist(), index=new_df.index)
-    new_df.drop(["pair1", "pair2", "index"], axis=1, inplace=True) 
-    new_df["pair_1"] = new_df[["danf1", "danf2"]].apply(lambda x: (x.danf1, x.danf2), axis=1)
-    new_df.drop(["danf1", "danf2"], axis=1, inplace=True)
-
-    new_df.to_parquet("../metadata/across_speaker_comparisons.parquet")
-
-
-# across speakers SMALL
-# this does not take forever :D
-comparisons = dict()
-NUM_PAIRS = 6
-# sorted_comparisons = set()
+# for each speaker at each age, pair a speech of theirs with a speech from NUM_PAIRS other speakers
 for ids1, dokid_anfnummers1 in ids_to_dokanf.items():
     for _ in range(NUM_PAIRS):
+        # find a random other speaker 
+        # (speaker is not the same as current speaker and the pair doesn't already exist in this order)
         ids2, dokid_anfnummers2 = choice(list(ids_to_dokanf.items()))
         while ids1[0] == ids2[0] or (ids1, ids2) in comparisons.keys():
             ids2, dokid_anfnummers2 = choice(list(ids_to_dokanf.items()))
-        
-        dokids = set([i.split("_")[0] for i in dokid_anfnummers1] + [i.split("_")[0] for i in dokid_anfnummers2])
-        danf1 = choice(dokid_anfnummers1)
-        danf2 = choice(dokid_anfnummers2)
-        if len(dokids) > 1:
-            while danf1.split("_")[0] == danf2.split("_")[0]:
-                danf1 = choice(dokid_anfnummers1)
-                danf2 = choice(dokid_anfnummers2)
-        pair = (danf1, danf2)
-        while pair in set(comparisons.values()):
-            danf1 = choice(dokid_anfnummers1)
-            danf2 = choice(dokid_anfnummers2)
-            if len(dokids) > 1:
-                while danf1.split("_")[0] == danf2.split("_")[0]:
-                    danf1 = choice(dokid_anfnummers1)
-                    danf2 = choice(dokid_anfnummers2)
-            pair = (danf1, danf2)
-        comparisons[(ids1, ids2)] = pair
+        #
+        # create a pair of speeches
+        # (possible it exists in the reverse order though)
+        comparisons[(ids1, ids2)] = get_pair(dokid_anfnummers1, dokid_anfnummers2)
 
+# save to a dataframe
 new_df = pd.DataFrame.from_dict(comparisons, columns=["danf1", "danf2"], orient="index")
 new_df = new_df.reset_index()
 new_df[["pair1", "pair2"]] = pd.DataFrame(new_df["index"].tolist(), index=new_df.index)
@@ -136,41 +110,59 @@ new_df.drop(["pair1", "pair2", "index"], axis=1, inplace=True)
 new_df["pair_1"] = new_df[["danf1", "danf2"]].apply(lambda x: (x.danf1, x.danf2), axis=1)
 new_df.drop(["danf1", "danf2"], axis=1, inplace=True)
 
-new_df.to_parquet("../metadata/across_speaker_comparisons_small.parquet")
+# new_df.to_parquet("../metadata/across_speaker_comparisons_for_within_age_comparisons.parquet")
+new_df.to_parquet("../metadata/across_speaker_comparisons.parquet")
+
+
+# skip starting age
+# sub_comparisons = dict(filter(lambda x: x[0][0][1] != ids_to_min_age[x[0][0][0]], comparisons.items()))
+# small_comparisons = dict()
+# for iids, danf_pair in sub_comparisons.items():
+#     iid_age1 = iids[0]
+#     if [x[0] for x in small_comparisons.keys()].count(iid_age1) < NUM_PAIRS // 2:
+#         small_comparisons[iids] = danf_pair
+
+# save to a dataframe
+# new_df = pd.DataFrame.from_dict(sub_comparisons, columns=["danf1", "danf2"], orient="index")
+# new_df = new_df.reset_index()
+# new_df[["pair1", "pair2"]] = pd.DataFrame(new_df["index"].tolist(), index=new_df.index)
+# new_df[["intressent_id1", "age1"]] = pd.DataFrame(new_df["pair1"].tolist(), index=new_df.index)
+# new_df[["intressent_id2", "age2"]] = pd.DataFrame(new_df["pair2"].tolist(), index=new_df.index)
+# new_df.drop(["pair1", "pair2", "index"], axis=1, inplace=True) 
+# new_df["pair_1"] = new_df[["danf1", "danf2"]].apply(lambda x: (x.danf1, x.danf2), axis=1)
+# new_df.drop(["danf1", "danf2"], axis=1, inplace=True)
+
+# new_df.to_parquet("../metadata/across_speaker_comparisons_for_across_age_comparisons.parquet")
+
 
 # within speaker start age vs other ages
-NUM_PAIRS = 6
+# create pairs for each speaker between their youngest age and any other age up to AGE_RANGE
+NUM_PAIRS = 3
 AGE_RANGE = 10
 
-df = pd.read_parquet("../metadata/bucketed_speeches.parquet")
-
-ids_to_dokanf = df.groupby(
-    ["intressent_id", "age"]).agg(list)["dokid_anfnummer"].to_dict()
-
 intressent_ids = set(ids[0] for ids in ids_to_dokanf)
+diff_age_pairs = defaultdict(set)
 
-pairs = defaultdict(set)
-
+# for each speaker, find their youngest age, generate speech pairs between that age+1 and any other age up to AGE_RANGE (including start_age)
+# each pair has NUM_PAIRS speeches associated with them
+# for now, some speakers have gap years due to a preprocessing error, so instead I choose to sort the speeches by age for each speaker
 for iid in intressent_ids:
     sub_ids_to_dokanf = dict(filter(lambda x: x[0][0] == iid, ids_to_dokanf.items()))
-    sub_ids_to_dokanf = dict(sorted(sub_ids_to_dokanf.items(), key=lambda x: x[0][1]))
-    start_age = min([i[1] for i in sub_ids_to_dokanf.keys()])
+    # sub_ids_to_dokanf = dict(sorted(sub_ids_to_dokanf.items(), key=lambda x: x[0][1]))
+    start_age = ids_to_min_age[iid]
+    dokid_anfnummers1 = ids_to_dokanf[(iid, start_age)]
     for sub_iid in sub_ids_to_dokanf.keys():
-        if sub_iid[1] >= start_age + AGE_RANGE:
-            break
+        # skip starting age
+        # if ids_to_min_age[iid] == sub_iid[1]:
+        #     continue
     # for age in range(start_age, start_age+AGE_RANGE+1):
-        for _ in range(NUM_PAIRS):
-            danf1 = choice(ids_to_dokanf[(iid, start_age)])
-            danf2 = choice(ids_to_dokanf[sub_iid])
-            while danf1 == danf2 or (danf1, danf2) in pairs[((iid, start_age), sub_iid)]:
-                danf1 = choice(ids_to_dokanf[(iid, start_age)])
-                danf2 = choice(ids_to_dokanf[sub_iid])
-            pairs[((iid, start_age), sub_iid)].add((danf1, danf2))
+        dokid_anfnummers2 = ids_to_dokanf[sub_iid]
+        while len(diff_age_pairs[((iid, start_age), sub_iid)]) < NUM_PAIRS:
+            diff_age_pairs[((iid, start_age), sub_iid)].add(get_pair(dokid_anfnummers1, dokid_anfnummers2))
+            
 
-print(set([len(pair) for pair in pairs.values()]))
-
-# new_df = pd.DataFrame(pairs, columns=["intressent_id", "age"] + [f"column_{i+1}" for i in range(6)])
-new_df = pd.DataFrame.from_dict(pairs, columns=[f"pair_{i+1}" for i in range(NUM_PAIRS)], orient="index")
+# save to a dataframe
+new_df = pd.DataFrame.from_dict(diff_age_pairs, columns=[f"pair_{i+1}" for i in range(NUM_PAIRS)], orient="index")
 new_df = new_df.reset_index()
 new_df[["sp1", "sp2"]] = pd.DataFrame(new_df["index"].tolist(), index=new_df.index)
 new_df[["intressent_id1", "age1"]] = pd.DataFrame(new_df["sp1"].tolist(), index=new_df.index)
@@ -179,18 +171,3 @@ new_df.drop(["sp1", "sp2", "index"], axis=1, inplace=True)
 new_df = new_df[["intressent_id1", "age1"] + ["intressent_id2", "age2"] + [f"pair_{i+1}" for i in range(NUM_PAIRS)]]
 
 new_df.to_parquet("../metadata/within_speaker_across_age_comparisons.parquet")
-
-subset = set(list(reduce(lambda x, y: list(x) + list(y), pairs.keys()))) # for if I want to keep only the iids+age from the previous step
-
-sub_comparisons = dict(filter(lambda x: x[0][0] in subset, comparisons.items()))
-
-new_df = pd.DataFrame.from_dict(sub_comparisons, columns=["danf1", "danf2"], orient="index")
-new_df = new_df.reset_index()
-new_df[["pair1", "pair2"]] = pd.DataFrame(new_df["index"].tolist(), index=new_df.index)
-new_df[["intressent_id1", "age1"]] = pd.DataFrame(new_df["pair1"].tolist(), index=new_df.index)
-new_df[["intressent_id2", "age2"]] = pd.DataFrame(new_df["pair2"].tolist(), index=new_df.index)
-new_df.drop(["pair1", "pair2", "index"], axis=1, inplace=True) 
-new_df["pair_1"] = new_df[["danf1", "danf2"]].apply(lambda x: (x.danf1, x.danf2), axis=1)
-new_df.drop(["danf1", "danf2"], axis=1, inplace=True)
-
-new_df.to_parquet("../metadata/across_speaker_comparisons_small.parquet")
