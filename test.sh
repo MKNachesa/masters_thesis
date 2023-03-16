fd1d3d770496041ed0afa92d55769f1475f11426
MKNachesa <maya.nachesa@gmail.com>
2023-03-16 15:00:03 +0100

    testing github

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git add .
+git commit -m "testing github"
+
+git push origin main
+MKNachesa
+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
